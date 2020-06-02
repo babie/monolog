71f40fad4529f695f08ca34b3785e540b9e3ca3e
babie <babie.wildheart@gmail.com>
2020-06-03 00:33:49 +0900

    chore: Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,15 @@
 module.exports = {
-  extends: ['airbnb-typescript'],
+  extends: [
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  plugins: ['prettier'],
   parserOptions: {
     project: './tsconfig.json',
   },
-};
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
