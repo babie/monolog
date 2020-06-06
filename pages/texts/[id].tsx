2205dd5eeb082b4dffe39d29784206ca3c9e15e1
babie <babie.wildheart@gmail.com>
2020-06-07 05:52:30 +0900

    refactor: Rename `postData` to `textData`

--- pages/texts/[id].tsx
+++ pages/texts/[id].tsx
@@ -2,14 +2,14 @@ import type { GetStaticPaths, GetStaticProps } from 'next'
 import { Layout } from '../../components/layout'
 import { getAllTextIds, getTextData, TextData, TextIds } from '../../lib/texts'
 
-type Props = { postData: TextData }
-const Post = ({ postData }: Props) => {
+type Props = { textData: TextData }
+const Post = ({ textData }: Props) => {
   return (
     <Layout>
       <article>
-        <h1>{postData.title}</h1>
-        <time>{postData.date}</time>
-        <p>{postData.body}</p>
+        <h1>{textData.title}</h1>
+        <time>{textData.date}</time>
+        <p>{textData.body}</p>
       </article>
     </Layout>
   )
@@ -26,11 +26,11 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const { id } = context.params
-  const postData: TextData = getTextData(id as string)
+  const textData: TextData = getTextData(id as string)
 
   return {
     props: {
-      postData,
+      textData,
     },
   }
 }
