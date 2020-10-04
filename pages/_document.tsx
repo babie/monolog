51d31c1bf012242a41917d3839f641fb976d8562
babie <babie.wildheart@gmail.com>
2020-10-05 01:20:19 +0900

    fix: Remove an unused import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { Layout } from '../components/layout'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
